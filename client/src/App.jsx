8b8f4e3563598a71ad2f358c6a184f876f087d7b
vishu <vishu.34343@gmail.com>
2024-08-10 15:14:59 +0530

    create header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import { BrowserRoute, Routes, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from './pages/Home';
 import About from './pages/About';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
+import Headers from './components/Headers';
 
 export default function App() {
-  return <BrowserRoute>
+  return (
+    <BrowserRouter>
+    {/* Header */}
+    <Headers />
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
@@ -15,5 +18,6 @@ export default function App() {
             <Route path="/sign-up" element={<SignUp />} />
             <Route path="/profile" element={<Profile />} />
         </Routes>
-    </BrowserRoute>;
+    </BrowserRouter>
+  );
 }
