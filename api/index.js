623222c9ddac84ea61c17587e8e5f7f75f58ba0a
vishu <vishu.34343@gmail.com>
2024-08-15 17:46:34 +0530

    middleware, error handler function

--- api/index.js
+++ api/index.js
@@ -14,12 +14,22 @@ mongoose.connect(process.env.MONGO).then(() => {
 const app = express();
 const port = 3000;
 
+app.listen(port, () => {
+    console.log(`server listning on the port ${port}`);
+});
+
 app.use(express.json());
 
 app.use('/api/user', userRouter);
 
 app.use('/api/auth', authRouter);
 
-app.listen(port, () => {
-    console.log(`server listning on the port ${port}`);
-});
+app.use((err, req, res, next) =>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal server error';
+    return res.status(statusCode).json({ 
+        success: false,
+        error: message,
+        statusCode,
+     });
+})
