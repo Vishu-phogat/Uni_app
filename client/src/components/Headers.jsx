107d278b90470784a4681329c54fc01c816bb613
vishu <vishu.34343@gmail.com>
2024-08-31 17:00:04 +0530

    update header and profile private

--- client/src/components/Headers.jsx
+++ client/src/components/Headers.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 export default function header() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <div className='bg-slate-200'>
       <div className='flex justify-between items-center max-w-7xl mx-auto p-4'>
@@ -11,7 +13,13 @@ export default function header() {
         <ul className='flex gap-4'>
         <Link to='/'><li>Home</li> </Link>
           <Link to='/about'><li>About</li></Link>
-          <Link to='/sign-in'><li>Sign In</li></Link>
+          <Link to='/profile'>
+            {currentUser? (
+              <img src={currentUser.profilePicture} alt='profile' className='h-7 w-7 rounded-full object-cover' />
+            ) : (
+            <li>Sign In</li>
+            )}
+            </Link>
         </ul>
       </div>
     </div>
