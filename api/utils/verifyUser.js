d7b16632b17ad5897319b47e5beb136355711a53
vishu <vishu.34343@gmail.com>
2024-09-13 13:11:42 +0530

    create update user api route

---
+++ api/utils/verifyUser.js
@@ -0,0 +1,18 @@
+// import { Jwt } from "jsonwebtoken";
+import jwt from 'jsonwebtoken';
+import { errorHandler } from './error.js';
+
+export const verifyToken = (req,res,next) => {
+    const token = req.cookies.access_token;
+
+    if(!token)
+        return next(errorHandler(401, "User is not authenticated"));
+
+    jwt.verify(token,process.env.JWT_SECRET, (err,user) => {
+        if(err) 
+            return next(errorHandler(403, "Token is not valid"));
+
+            req.user = user;
+            next();
+    });
+}
